5dcf9e942ad11d276341b05bfd5af8057c5367fb
saikadepu <saikumaradepu@gmail.com>
2019-11-26 12:55:47 -0500

    Displaying Quiz Questions and Handling Events

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -4,11 +4,14 @@ class QuizQuestionButton extends Component{
     render() {
         return(
             <li>
-                <button>
+                <button onClick={this.handleClick.bind(this)}>
                     {this.props.button_text}
                 </button>
             </li>
         )
+    }
+    handleClick(){
+        this.props.clickHandler(this.props.button_text)
     }
 }
 
